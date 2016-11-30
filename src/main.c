f0976e79a404112a57ff42cd3b7f689844d8dd6a
William WATKINS <wwatkins@e2r4p9.42.fr>
2016-11-30 14:35:16 +0100

    vklepper, stop watching my github pls ;)

--- src/main.c
+++ src/main.c
@@ -6,25 +6,11 @@
 /*   By: wwatkins <wwatkins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/29 17:20:21 by wwatkins          #+#    #+#             */
-/*   Updated: 2016/11/30 14:28:20 by wwatkins         ###   ########.fr       */
+/*   Updated: 2016/11/30 14:33:38 by wwatkins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "scop.h"
-
-GLfloat vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
-};
-
-// Creates a VBO (vertex buffer object), it will be on GPU memory.
-GLuint VBO;
-glGenBuffer(1, &VBO);
-// Binds a VBO to a type of buffer.
-glBindBuffer(GL_ARRAY_BUFFER, VBO);
-// Copy user-defined data to the specified buffer type (here, GL_ARRAY_BUFFER).
-glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 int		main(int argc, char **argv)
 {
@@ -34,6 +20,21 @@ int		main(int argc, char **argv)
 	glfw_init_win(&e, 1280, 720);
 
 	glfwSetKeyCallback(e.win.ptr, key_callback);
+
+	GLfloat vertices[] = {
+	    -0.5f, -0.5f, 0.0f,
+	     0.5f, -0.5f, 0.0f,
+	     0.0f,  0.5f, 0.0f
+	};
+
+	// Creates a VBO (vertex buffer object), it will be on GPU memory.
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	// Binds a VBO to a type of buffer.
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// Copy user-defined data to the specified buffer type (here, GL_ARRAY_BUFFER).
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
 	while(!glfwWindowShouldClose(e.win.ptr))
 	{
 		glfwPollEvents();
